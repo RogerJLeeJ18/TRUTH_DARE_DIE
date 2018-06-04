3f52572a31c7df155e6f4f97c0640e33370efb28
Taylor Shephard <lasha.tay@gmail.com>
2018-06-04 11:23:10 -0500

    server handle sign up

--- Database/mongoose.js
+++ Database/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://<admin>:<admin1>@ds243728.mlab.com:43728/truthdaredie');
+mongoose.connect('mongodb://admin:admin1@ds243728.mlab.com:43728/truthdaredie');
 
 const db = mongoose.connection;
 
@@ -8,3 +8,47 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('connected');
 });
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: String,
+  id_token: String,
+  avatar: String,
+  email: String,
+  save_tokens: Number,
+  death_tokens: Number,
+});
+
+const User = mongoose.model('User', UserSchema);
+
+const save = (user) => {
+  const newUser = new User({
+    username: user.name,
+    id_token: user.id,
+    avatar: user.image_url,
+    email: user.email,
+    save_tokens: 0,
+    death_tokens: 0,
+  });
+
+  newUser.save((err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('saved');
+    }
+  });
+};
+
+const findUser = (user) => {
+  User.findOne({ email: user.email }, (err, data) => {
+    if (err) {
+      console.error(err);
+    }
+    return true;
+  });
+};
+
+module.exports.findUser = findUser;
+module.exports.save = save;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,34 @@
 const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const dataSave = require('../Database/mongoose');
+
 
 const app = express();
 
 app.use(express.static(`${__dirname}/../client`));
 app.use(express.static(`${__dirname}/../database`));
+app.use(bodyParser.json());
 
-app.get('', (req, res) => {
+app.get('/users', (req, res) => {
 
 });
 
-app.post('', (req, res) => {
-
+// post request to database for sign up
+app.post('/users', (req, res) => {
+  // request('/users', (err, response, body) => {
+  //   if (err) {
+  //     console.error(err);
+  //   }
+  const data = req.body;
+  console.log(data);
+  if (dataSave.findUser(data)) {
+    res.send('User Already Exists');
+  } else {
+    dataSave.save(data);
+    res.send('Welcome');
+  }
+  // });
 });
 
 app.listen(3000, () => {
