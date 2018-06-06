dee6a83d84d0e76051f826a0761ca3cd57559b9e
Andy Nguyen <andyn190@gmail.com>
2018-06-06 13:06:57 -0500

    working signup and login pages to post and get from the server

---
+++ client/app/login.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function Login(props) {
+  const element = (
+    <div className="container">
+      <div className="row">
+        <div className="col-xs-10 col-xs-offset-1">
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            props.login(e);
+          }}
+          >
+            <label htmlFor="username">Username:
+              <input type="text" name="username" />
+            </label>
+            <br />
+            <label htmlFor="password">Password:
+              <input type="password" name="password" />
+            </label>
+            <br />
+            <button type="submit" >Submit</button>
+            <br />
+            <button onClick={(e) => {
+              e.preventDefault();
+              props.signUpButton(e);
+            }}
+            >
+            Sign up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+  return element;
+}
+
+export { Login };
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
 const path = require('path');
 
 const DIST_DIR = path.resolve(`${__dirname}`, 'dist');
-const SRC_DIR = path.resolve(`${__dirname}`, 'client');
 
 const config = {
-  devtool: 'cheap-eval-source-map',
+  devtool: 'cheap-module-source-map',
   mode: 'development',
-  entry: [`${SRC_DIR}/app/index.jsx`, `${SRC_DIR}/app/chatroom.jsx`],
+  entry: ['./client/app/index.jsx'],
   output: {
     path: `${DIST_DIR}/app`,
     filename: 'bundle.js',
     publicPath: '/app/',
+    library: 'bundle.js',
+    libraryTarget: 'window',
+    libraryExport: 'default',
   },
   module: {
     rules: [
       {
-        test: /\.js?/,
-        include: SRC_DIR,
+        test: /\.jsx?/,
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015', 'stage-2'],
