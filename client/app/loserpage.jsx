4fc3f75701a3724f690eb1b6db41332a0c2de8bf
Taylor Shephard <lasha.tay@gmail.com>
2018-06-09 23:19:38 -0500

    finished loser page and winner page

--- client/app/loserpage.jsx
+++ client/app/loserpage.jsx
@@ -6,70 +6,12 @@ const Message = styled.h1`
     margin: auto;
     width: 50%;
     padding: 10px;
+    text-align: center;
 `;
-
-// const Deaths = styled.li`
-//   color: red;
-//   font-size: 22px;
-//   list-style-type: none;
-//   float: right;
-//   display: block;
-//   text-align: center;
-//   padding: 14px 16px;
-//   background-color: black;
-// `;
-
-// const Saves = styled.li`
-//   font-size: 22px;
-//   list-style-type: none;
-//   float: right;
-//   display: block;
-//   text-align: center;
-//   padding: 14px 16px;
-//   background-color: gold;
-// `;
-
-// const Wins = styled.li`
-//   font-size: 22px;
-//   list-style-type: none;
-//   float: right;
-//   display: block;
-//   text-align: center;
-//   padding: 14px 16px;
-//   background-color: white;
-// `;
-
-// const User = styled.li`
-//   color: white;
-//   font-size: 22px;
-//   list-style-type: none;
-//   float: left;
-//   display: block;
-//   text-align: center;
-//   padding: 14px 16px;
-// `;
-
-// const TopBar = styled.ul`
-//   list-style-type: none;
-//   margin: 0;
-//   padding: 0;
-//   overflow: hidden;
-//   background-color: gray;
-//   overflow: hidden;
-//   position: fixed;
-//   top: 0;
-//   width: 100%;
-// `;
 
 function LoserPage() {
   return (
     <div>
-    {/* <TopBar className="userInfo">
-      <User>Hello, {this.props.userInfo.username}</User>
-      <Saves>Saves: {this.props.userInfo.save_tokens}</Saves>
-      <Deaths>Deaths: {this.props.userInfo.death_tokens}</Deaths>
-      <Wins>Wins: {this.props.userInfo.win_tokens}</Wins>
-    </TopBar> */}
     <Message>HA HA You're Dead</Message>
     </div>
   );
--- client/app/winnerpage.jsx
+++ client/app/winnerpage.jsx
@@ -6,12 +6,16 @@ const Message = styled.h1`
     margin: auto;
     width: 50%;
     padding: 10px;
-    text-align: justify;
+    text-align: center;
 `;
 
 function WinnerPage() {
   return (
-    <Message>Congratulations, You Survived!... For Now</Message>
+    <div>
+    <Message>Congratulations,</Message>
+    <Message>You Survived!...</Message>
+    <Message>For Now</Message>
+    </div>
   );
 }
 
