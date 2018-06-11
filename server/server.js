70878a2d4a79c354dbec37d6a38c8cb03362ba2c
Andy Nguyen <andyn190@gmail.com>
2018-06-11 14:28:23 -0500

    another change to see if this works

--- server/server.js
+++ server/server.js
@@ -229,6 +229,6 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 5000;
 
 
-server.listen(PORT, 'localhost', () => {
+server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
